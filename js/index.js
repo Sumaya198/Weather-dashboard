c60671a33a16aea31fded2d9cc7bd54125d86d79
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2020-10-23 18:33:01 +0100

    third commit

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const searchBox = document.querySelector('.btn');
-const cityValue = document.querySelector('.search-location input')
-
-//event listener
-searchBox.addEventListener('submit', e => { 
-   e.preventDefault();
-    const citySearched = cityValue.value.trim();
-    console.log(citySearched);
-})
